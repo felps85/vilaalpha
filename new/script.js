8d36f25d05c3b83b5b501afd90e0978b687c755f
Luiz Felipe Garcia Soares <fe@Luizs-MacBook-Pro.local>
2018-05-15 22:18:50 +0100

    add new site

---
+++ new/script.js
@@ -0,0 +1,61 @@
+function initMap() {
+  var markerArray = [];
+
+  // Instantiate a directions service.
+  var directionsService = new google.maps.DirectionsService;
+
+  // Create a map and center it on Manhattan.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {
+      lat: -23.4439093,
+      lng: -46.8995931
+    }
+  });
+
+  // Create a renderer for directions and bind it to the map.
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  // Instantiate an info window to hold step text.
+  var stepDisplay = new google.maps.InfoWindow;
+
+  // Display the route between the initial start and end selections.
+  calculateAndDisplayRoute(
+    directionsDisplay, directionsService, markerArray, stepDisplay, map);
+  // Listen to change events from the start and end lists.
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(
+      directionsDisplay, directionsService, markerArray, stepDisplay, map);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+
+}
+
+function calculateAndDisplayRoute(directionsDisplay, directionsService,
+  markerArray, stepDisplay, map) {
+  // First, remove any existing markers from the map.
+  for (var i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(null);
+  }
+
+  // Retrieve the start and end locations and create a DirectionsRequest using
+  // DRIVING directions.
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: 'School Vila Alpha',
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    // Route the directions and pass the response to a function to create
+    // markers for each step.
+    if (status === 'OK') {
+      document.getElementById('warnings-panel').innerHTML =
+        '<b>' + response.routes[0].warnings + '</b>';
+      directionsDisplay.setDirections(response);
+      showSteps(response, markerArray, stepDisplay, map);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
